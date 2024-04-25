ebd5864630fefca1266f014dea17306e49e56353
xbrayan0212 <xbrayan.tobii02@gmail.com>
2024-04-25 09:12:59 -0500

    correcciones de codigo

--- paginas/cheques.php
+++ paginas/cheques.php
@@ -7,9 +7,6 @@
     <title>Document</title>
     <script src="../scripts/validaciones.js" defer></script>
     <script src="../scripts/script.js" defer></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Reddit+Mono:wght@200..900&display=swap" rel="stylesheet">
 </head>
 <body>
 
